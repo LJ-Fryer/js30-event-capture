e04d12c5b0293e9c9f45d23ba4ab1cab525d31c8
LJ-Fryer <ljf_aus@hotmail.com>
2025-06-10 18:58:50 +1000

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Testing");
+const divs = document.querySelectorAll("div");
+// const button = document.querySelector("button");
+
+function logText(e) {
+  console.log(this.classList.value);
+  e.stopPropagation(); // Stop bubbling
+}
+divs.forEach((div) => div.addEventListener("click", logText));
+// button.addEventListener("click",() => {console.log("It works");}, { once: true });
